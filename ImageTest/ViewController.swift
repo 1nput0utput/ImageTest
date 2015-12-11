c149e437d465d169373da2e4e17a2b99818c2b0f
Rajinder Ramgarhia <rramgarhia@vandewater.com>
2015-12-11 15:39:20 -0500

    Initial Commit

---
+++ ImageTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageTest
+//
+//  Created by Rajinder Ramgarhia on 2015-12-11.
+//  Copyright © 2015 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
